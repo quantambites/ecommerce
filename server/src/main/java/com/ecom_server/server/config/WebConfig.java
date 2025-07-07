e126b6862ce6c81736767915810af81bb0999d40
crashmovies <swarnakarharshendu420@gmail.com>
2025-07-07 21:24:15 +0530

    user

--- server/src/main/java/com/ecom_server/server/config/WebConfig.java
+++ server/src/main/java/com/ecom_server/server/config/WebConfig.java
@@ -8,13 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${spring.frontend.origin}")
-    private String frontendOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendOrigin) // your frontend origin
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://ecommerce-d6069.web.app",
+                        "https://ecommerce-d6069.firebaseapp.com"
+                ) // your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
